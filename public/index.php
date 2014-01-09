c0bfc81717bf7fc3a1238ebd7417d6943161ca91
root <root@vagrant-debian-squeeze.vagrantup.com>
2014-01-09 10:38:01 -0200

    PHPmailer settings

--- public/index.php
+++ public/index.php
@@ -12,16 +12,10 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 ));
 
 // register swiftmailer
-$app->register(new Silex\Provider\SwiftmailerServiceProvider());
-$app['swiftmailer.options'] = array(
-    'host' => MAIL_HOST,
-    'port' => MAIL_PORT,
-    'username' => MAIL_USER,
-    'password' => MAIL_PASS,
-    'encryption' => 'ssl',
-    'auth_mode' => true
-);
-
+$transport = Swift_SmtpTransport::newInstance(MAIL_HOST, MAIL_PORT, 'ssl')
+	        ->setUsername(MAIL_USER)
+	        ->setPassword(MAIL_PASS);
+$app['swiftmailer'] = Swift_Mailer::newInstance($transport);
 
 // Application
 $app->get('/', function () use ($app) {
@@ -31,14 +25,15 @@ $app->get('/', function () use ($app) {
 $app->post('/send', function () use ($app) {
 	try{
 		$request = $app['request'];
+		$mailer = $app['swiftmailer'];
 
 		$name = $app->escape($request->get('name'));
 		$email = $app->escape($request->get('email'));
 		$message = $app->escape($request->get('message'));
 
 		$body = sprintf("Name: %s\nEmail: %s\nMessage: %s", $name, $email, $message);
-
-	    $message = \Swift_Message::newInstance()
+	
+	    $message = Swift_Message::newInstance()
 	        ->setSubject('[Startup Dev] Contact')
 	        ->setFrom(array( $email ))
 	        ->setTo(array( MAIL_USER ))
@@ -46,7 +41,7 @@ $app->post('/send', function () use ($app) {
 
 
 	   	$result = array('success' => false, 'msg' => 'Email not sent.' );
-	    if( $app['mailer']->send($message) ){
+	    if( $mailer->send($message) ){
 	    	$result = array('success' => true, 'msg' => 'OK, we will return ASAP =)' );
 	    }
 	   	die(json_encode($result));
